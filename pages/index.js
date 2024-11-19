f6f915680ea5cdc77b118336e7e4b1e6eca5cb54
Manimaran T <maran.mallow@gmail.com>
2024-11-19 10:55:50 +0000

    init: basic setup 2

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,7 @@ import Button from '../components/Button'
 
 function Home() {
   return (
-    <main className={styles.main}>
-      <h2 className="text-2xl ml-6 mt-6 tracking-wide font-bold">The Prelyst</h2>
-      {/* <hr className={styles.hr} /> */}
-    </main>
+      <h2 className="m-2">Hello, world</h2>
   )
 }
 
